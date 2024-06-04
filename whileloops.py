4b2ae9d17c1f87d21c625e1c2e843c1619db361f
Jcasanova1990 <jeremykcasanova@gmail.com>
2024-06-04 13:22:55 -0400

    whileloops

---
+++ whileloops.py
@@ -0,0 +1,11 @@
+counter = 0
+
+while (counter < 10):
+    print(counter)
+    counter += 1
+
+    for i in range(10):
+                    if i % 2 == 0:
+                        print("it's even")
+                    elif i % 3 == 0:
+                            print("meow")
